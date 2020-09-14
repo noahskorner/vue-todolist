bbe1a0be090d4efde2e097ae53a8f854a801bbb3
Noah Korner <noahskorner@gmail.com>
2020-09-14 12:07:56 -0700

    Created todo list application, uploading files.

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add todo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+//import { v4 as uuid } from "uuid";
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo(e) {
+      //stop page from submitting automatically
+      e.preventDefault();
+      const newTodo = {
+        title: this.title,
+        completed: false,
+      };
+
+      //send this to parent
+      this.$emit("add-todo", newTodo);
+
+      //clear input field
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
